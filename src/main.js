68f6d4a90abada2c4edb90a5ee83c4d36535d3e7
JingYuan <JingYuanan_4@163.com>
2020-03-15 11:55:34 +0800

    rem适配完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant'// 引入vant组件
 import 'vant/lib/index.css' // 引入vant组件样式
+import 'amfe-flexible'
 Vue.use(Vant) // 全局注册vant
 Vue.config.productionTip = false
 new Vue({
