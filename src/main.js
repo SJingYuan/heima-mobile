f84dc469cc717a96cc99756e14b733987b5a01ff
JingYuan <JingYuanan_4@163.com>
2020-03-18 10:54:56 +0800

    登陆-表单校验

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import '@/permission'
 import store from './store'
 import Vant from 'vant'// 引入vant组件
-import 'vant/lib/index.css' // 引入vant组件样式
+import 'vant/lib/index.less'
+// import 'vant/lib/index.css' // 引入vant组件样式
 import 'amfe-flexible'
 import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
 Vue.use(Vant) // 全局注册vant
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,28 +1,86 @@
 <template>
-  <div class='container'>
+  <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
     <!-- 手机号  验证码  登录按钮 -->
     <van-cell-group>
-      <van-field  label="手机号" placeholder="请输入手机号"></van-field>
-      <van-field  label="验证码" placeholder="请输入验证码">
+      <van-field
+      @blur="checkMobile"
+        :error-message="errorMessage.mobile"
+        v-model.trim="loginForm.mobile"
+        label="手机号"
+        placeholder="请输入手机号"
+      ></van-field>
+      <van-field
+      @blur="checkCode"
+        :error-message="errorMessage.code"
+        v-model.trim="loginForm.code"
+        label="验证码"
+        placeholder="请输入验证码"
+      >
         <!-- slot指定给哪个坑填内容 -->
-        <van-button  slot="button" size="small" type="primary">发送验证码</van-button>
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
       </van-field>
     </van-cell-group>
-    <div class='btn-box'>
-      <van-button type="info" size="small" round block>登录</van-button>
+    <div class="btn-box">
+      <van-button @click="login" type="info" size="small" round block>登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  data () {
+    return {
+      loginForm: {
+        mobile: '13911111111', // 手机号
+        code: '246810' // 验证码
+      },
+      errorMessage: {
+        mobile: '', // 手机错误消息
+        code: '' // 验证码
+      }
+    }
+  },
+  methods: {
+    // 检查手机号
+    checkMobile () {
+      // 判断是否为空
+      if (!this.loginForm.mobile) {
+        this.errorMessage.mobile = '手机号不能为空'
+        return false
+      }
+      // 判断手机号格式
+      if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
+        this.errorMessage.mobile = '手机号格式不正确'
+        return false
+      }
+      this.errorMessage.mobile = ''
+      return true
+    },
+    // 检查验证码
+    checkCode () {
+      if (!this.loginForm.code) {
+        this.errorMessage.code = '验证码不能为空'
+        return false
+      }
+      if (!/^\d{6}$/.test(this.loginForm.code)) {
+        this.errorMessage.code = '验证码必须是六位数字'
+        return false
+      }
+      this.errorMessage.code = ''
+      return true
+    },
+    login () {
+      if (this.checkMobile() && this.checkCode()) {
+
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-   .btn-box {
-     padding: 20px;
-   }
+.btn-box {
+  padding: 20px;
+}
 </style>
